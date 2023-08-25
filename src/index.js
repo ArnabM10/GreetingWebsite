efc5ccbe07e8aaad7a3bc14a25704e6fd044cd7d
Arnab Manna <arnabmanna221@gmail.com>
2023-08-25 19:53:02 +0530

    index.js updated

--- src/index.js
+++ src/index.js
@@ -1,33 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-
-let currDate = new Date();
-currDate = currDate.getHours();
-
-let greeting = ' ';
-const cssStyle = { };
-if(currDate >= 1 && currDate < 12){
-   greeting = 'Good Morning';
-  cssStyle.color = 'Green';
-}else if(currDate > 12 && currDate < 19){
-  greeting = 'Good Afternoon';
-  cssStyle.color = 'Orange';
-}else{
-  greeting = 'Good Night';
-  cssStyle.color = 'Black';
-}
-
+import './index.css'
+import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
-    <div>
-      <h1>
-        Hello sir,
-        <span style = {cssStyle}>{greeting}</span>
-      </h1>
-    </div>
-  </>
+    <App/>
 );
 
